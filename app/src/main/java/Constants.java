56ceab162d329d172a63952efe8a267ccd368b33
AuraLoredana <popescuauraloredana@gmail.com>
2018-06-02 09:32:04 +0300

    Constants deleted

--- app/src/main/java/Constants.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by POPESCU on 6/1/2018.
- */
-
-public class Constants {
-    public static final String API_POPULAR_MOVIES_BASE_URL = "https://api.themoviedb.org/3/movie/popular?";
-    public static final String API_TOP_RATED_MOVIES_BASE_URL = "https://api.themoviedb.org/3/movie/top_rated?";
-    public static final String API_POSTER_MOVIES_BASE_URL = "http://image.tmdb.org/t/p/";
-}
